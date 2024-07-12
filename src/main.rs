8c96a2a217efb00e68620c65a78e6884e0243f60
BishwasXD <gautambishwas7@gmail.com>
2024-07-12 21:49:03 +0545

    playing with strings

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,15 @@ fn main() {
     let limit: u8 = 240;  //i8 throws error
     
     let float: f64 = 22.0 / 7.0; //one bit for sign, 8 for exponent, 23 for mantissa, 32 provides 7 decimal digits of precision, 15 for 64, doint just 22/7 cuts the decimal and treats the value as interger 3 in this case
+    let a: char = 'a'; //char takes 32 bit
+    let name: &str = "John Doe";
     
+    let address: String = "kathmandu"
+    println!("the address is {}", address)
+
+    println!("The name is {}", name); 
+
+    println!("printing character {}", a);
     println!("checking floating number {}", float);
     println!("checking limit of signed and unsigned {}", limit);
     println!("not a positive number {}", positive);
@@ -28,4 +36,11 @@ fn main() {
         }
         println!("{} is the number", number);
     }
+    let result = add(3,4);
+    println!("result is {}", result);
+}
+
+fn add(x: i8, y:i8) -> i8{
+    println!("inside of add function");
+   return  x + y;
 }
